316e05470c4066c3ff7b9e63b48ca864780efa9a
Trey Herrera <therrera152@gmail.com>
2022-02-28 10:18:09 -0600

    commit tree 93bd076972afa1e26ad333793471e0ab065489bb

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Write your solution in this file!
+const employee = {
+    name: "Sam",
+     streetAddress: "11 Broadway",
+};    
+function updateEmployeeWithKeyAndValue (employee, key, value) {
+    return {...employee,...{[key]: value}};
+}  
+// returns an employee wit the original key value pairs and the
+//new key value pair non destructively
+
+function destructivelyUpdateEmployeeWithKeyAndValue (employee, key, value){
+    employee[key]=value;
+    return employee;
+}
+//it update employee with given key and value destructively
+//returns entire updated employee
+
+function deleteFromEmployeeByKey (employee, key){
+    let newEmployee = {...employee};
+    delete newEmployee[key];
+    return newEmployee;
+}
+//delete key from a clone of employee and returns the new employee
+//does not modify the original employee (it is non-destructive)
+
+function destructivelyDeleteFromEmployeeByKey (employee,key){
+    delete employee[key];
+    return employee;
+}
